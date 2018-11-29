ea987c49098ca295c66f9408584b8304fcfc7971
Michael Alvin <michael.alvin@stonybrook.edu>
2018-11-29 00:43:03 -0500

    Cleaned up errors

--- populate_table.sql
+++ populate_table.sql
@@ -3,7 +3,7 @@
 
 #Creed movie
 INSERT INTO Movie (movieName, rating, parentalRating, releaseDate) 
-VALUES ("Creed", 8.2, "PG-13", 2015-11-25);
+VALUES ("Creed", 8.2, "PG-13", "2015-11-25");
 
 INSERT INTO Person (lastName, firstName, age) 
 VALUES ("Coogler", "Ryan", 32);
@@ -17,31 +17,24 @@ INSERT INTO Person (lastName, firstName, age)
 VALUES ("Stallone", "Sylvester", 65);
 INSERT INTO Actor VALUES ((SELECT LAST_INSERT_ID()));
 
-INSERT INTO Casts (directorId,producerId,movieId)
-VALUES ((SELECT MAX(ID) FROM Director), (SELECT MAX(ID) FROM Producer), (SELECT MAX(ID) FROM Movie));
+INSERT INTO Person (lastName, firstName, age) 
+VALUES ("John", "Smith", 1242);
+INSERT INTO Producer VALUES ((SELECT LAST_INSERT_ID()));
 
-INSERT INTO CastsActor
-VALUES ((SELECT ID FROM Person WHERE lastName = 'Stallone' AND firstName = 'Sylvester' LIMIT 1),
-		(SELECT ID FROM Movie WHERE movieName = 'Creed' LIMIT 1));
-        
-INSERT INTO CastsActor
-VALUES ((SELECT ID FROM Person WHERE lastName = 'Jordan' AND firstName = 'Michael' LIMIT 1),
-		(SELECT ID FROM Movie WHERE movieName = 'Creed' LIMIT 1));
-
+INSERT INTO Casts (directorId,producerId,movieId,actorID)
+VALUES ((SELECT MAX(ID) FROM Director), (SELECT MAX(ID) FROM Producer), (SELECT MAX(ID) FROM Movie), (SELECT MAX(ID) FROM Actor));
 
 #Creed2 Movie
 INSERT INTO Movie (movieName, rating, parentalRating, releaseDate) 
-VALUES ("Creed 2", 7.35, "PG-13", 2018-12-10);
+VALUES ("Creed 2", 7.35, "PG-13", "2018-12-10");
 
 INSERT INTO Person (lastName, firstName, age)
-SELECT * FROM (SELECT 'Jordan', 'Michael', 44) AS tmp
+SELECT * FROM (SELECT 'Jordan', 'Michael', 44) AS tmMoviep
 WHERE NOT EXISTS (
     SELECT firstName,lastName 
 	FROM Person
 	WHERE firstName = 'Michael' AND lastName = 'Jordan' AND age = 44
 ) LIMIT 1;
 
-INSERT INTO Actor
-SELECT * FROM (SELECT 
 
 
