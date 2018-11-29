2931f8636777902308b84080d4a174b4ddeeb431
bfeng12 <benny.feng@stonybrook.edu>
2018-11-28 20:59:30 -0500

    creating an sql script to populate your database

---
+++ populate_table.sql
@@ -0,0 +1,10 @@
+#SQL Script for Populating our table
+
+#Creed movie
+INSERT INTO Movie (movieName, rating, parentalRating, releaseDate) 
+VALUES ("Creed", 8.2, "PG-13", 2015-11-25);
+
+INSERT INTO Person (lastName, firstName, age) 
+VALUES ("Coogler", "Ryan", 32);
+
+INSERT INTO Director VALUES 
