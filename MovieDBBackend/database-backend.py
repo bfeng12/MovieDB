d7301b34377c925e7ac418c45af3500ca97a34d7
bfeng12 <benny.feng@stonybrook.edu>
2018-11-28 23:50:37 -0500

    changed these stuffs

--- MovieDBBackend/database-backend.py
+++ MovieDBBackend/database-backend.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify, make_response, request, abort
 import mysql.connector
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app=Flask(__name__)
-CORS(app)
+#CORS(app)
 
 db = mysql.connector.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
-                     passwd="together5",  # your password
+                     passwd="awesome",  # your password
                      db="movieDB")        # name of the data base
 dbcursor = db.cursor()
 
@@ -20,13 +20,51 @@ def test():
 # ######################
 # ##ALL THE MOVIE ONES##
 # ######################
+def movie_to_json(fetchall):
+    # Turns an array returned from fetchallMethod() into a json file
+    resultList = []
+    for x in fetchall:
+        tempDict = {
+            'id': x[0],
+            'movieName': x[1],
+            'rating': x[2],
+            'parentalRating': x[3],
+            'releaseDate': x[4]
+        }
+        resultList.append(tempDict)
+    return resultList
+
+
+@app.route('/moviedb/movie/rating/<int:rating>', methods=['GET'])
+def get_movies_rating(rating):
+    # Gets movies where rating is greater than X
+    query = "SELECT * FROM Movie WHERE rating >= (%s)"
+    dbcursor.execute(query, [rating]);
+    return jsonify(movie_to_json(dbcursor.fetchall()));
+
+
 @app.route('/moviedb/movie/actor/<string:actor>', methods=['GET'])
 def get_movies_actor(actor):
     # Gets the list of movies and actor has acted in
-    query = "SELECT Movie.movieName " \
-            "FROM Movie, CastsActor, Actor" \
-            "WHERE Actor.firstName = (%s) AND Movie.ID = CastsActor.movieID AND Actor.ID = CastsActor.actorID"
-    dbcursor.execute(query,actor)
+    query = \
+    "SELECT DISTINCT firstName, Movie.ID, Movie.movieName, Movie.rating, Movie.parentalRating, Movie.releaseDate "\
+    "FROM Movie, CastsActor, Actor, Person "\
+    "WHERE Person.firstName = (%s) AND Movie.ID = CastsActor.movieID " \
+    "AND Actor.ID = actorID"
+
+    dbcursor.execute(query,[actor])
+    resultList = []
+    for x in dbcursor.fetchall():
+        tempDict = {
+            'actorName': x[0],
+            'id': x[1],
+            'movieName': x[2],
+            'rating': x[3],
+            'parentalRating': x[4],
+            'releaseDate': x[5]
+        }
+        resultList.append(tempDict)
+    return jsonify(resultList)
 
 
 @app.route('/moviedb/movie/year/<int:year>', methods=['GET'])
@@ -50,17 +88,7 @@ def get_movies_year(year):
 def get_movies():
     # Returns a json file of all movies in the MovieDB
     dbcursor.execute("SELECT * FROM Movie")
-    resultList = []
-    for x in dbcursor.fetchall():
-        tempDict = {
-            'ID': x[0],
-            'Movie Name': x[1],
-            'Rating': x[2], 
-            'Parental Rating': x[3],
-            'Release Date': x[4]
-        }
-        resultList.append(tempDict)
-    return jsonify(resultList) 
+    return jsonify(movie_to_json(dbcursor.fetchall()))
 
 @app.errorhandler(404)
 def not_found(error):
@@ -107,16 +135,6 @@ def get_persons():
         resultList.append(empDict)
     return jsonify(resultList) 
 
-
-def array_to_json(arrays):
-    result = ""
-    for x in arrays:
-        temp = ""
-        temp += "\"id\": " + x[0] + ",\n"
-        temp += "\"firstName\": " + x[1] + ",\n"
-        temp += "\"lastName:\" " + x[2] + ",\n"
-        temp += "\"age\"" + x[3] + ",\n"
-        result += temp
 
 if __name__ == '__main__':
     app.run(debug=True)
--- populate_table.sql
+++ populate_table.sql
@@ -1,4 +1,5 @@
 #SQL Script for Populating our table
+#By Benny Feng
 
 #Creed movie
 INSERT INTO Movie (movieName, rating, parentalRating, releaseDate) 
@@ -6,5 +7,41 @@ VALUES ("Creed", 8.2, "PG-13", 2015-11-25);
 
 INSERT INTO Person (lastName, firstName, age) 
 VALUES ("Coogler", "Ryan", 32);
+INSERT INTO Director VALUES ((SELECT LAST_INSERT_ID()));
 
-INSERT INTO Director VALUES 
+INSERT INTO Person (lastName, firstName, age) 
+VALUES ("Jordan", "Michael", 44);
+INSERT INTO Actor VALUES ((SELECT LAST_INSERT_ID()));
+
+INSERT INTO Person (lastName, firstName, age) 
+VALUES ("Stallone", "Sylvester", 65);
+INSERT INTO Actor VALUES ((SELECT LAST_INSERT_ID()));
+
+INSERT INTO Casts (directorId,producerId,movieId)
+VALUES ((SELECT MAX(ID) FROM Director), (SELECT MAX(ID) FROM Producer), (SELECT MAX(ID) FROM Movie));
+
+INSERT INTO CastsActor
+VALUES ((SELECT ID FROM Person WHERE lastName = 'Stallone' AND firstName = 'Sylvester' LIMIT 1),
+		(SELECT ID FROM Movie WHERE movieName = 'Creed' LIMIT 1));
+        
+INSERT INTO CastsActor
+VALUES ((SELECT ID FROM Person WHERE lastName = 'Jordan' AND firstName = 'Michael' LIMIT 1),
+		(SELECT ID FROM Movie WHERE movieName = 'Creed' LIMIT 1));
+
+
+#Creed2 Movie
+INSERT INTO Movie (movieName, rating, parentalRating, releaseDate) 
+VALUES ("Creed 2", 7.35, "PG-13", 2018-12-10);
+
+INSERT INTO Person (lastName, firstName, age)
+SELECT * FROM (SELECT 'Jordan', 'Michael', 44) AS tmp
+WHERE NOT EXISTS (
+    SELECT firstName,lastName 
+	FROM Person
+	WHERE firstName = 'Michael' AND lastName = 'Jordan' AND age = 44
+) LIMIT 1;
+
+INSERT INTO Actor
+SELECT * FROM (SELECT 
+
+
